f3ada5647929e7442a1c58f1e7d3ce0254a4d55b
Garbs1 <villegas.garcia.gabriela11@gmail.com>
2023-12-13 01:15:42 -0600

    Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,28 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div className="bg-purple h-screen flex justify-center items-center">
-      <section className="flex flex-col items-center">
+    <div className="bg-purple h-screen flex flex-col justify-between">
+      <section className="flex flex-col items-center mt-16">
         <h1 className="text-5xl font-irish text-white">
           Bienvenido a Brain Brawl
         </h1>
         <form
-          className="flex flex-col items-center justify-center bg-peach w-full max-w-xs"
+          className="flex flex-col items-center justify-center bg-peach w-full max-w-xs p-7 mt-20 gap-y-4 rounded-lg"
           action=""
         >
-          <input type="text" placeholder="Apodo" />
-          <button className="bg-pink">Todo Listo</button>
+          <div className="w-20 h-20 bg-zinc-700 rounded-full"></div>
+          <p className="text-center px-9 font-montserrat text-lg">
+            {" "}
+            ¡Sumérgete en la batalla de las mentes más poderosas!{" "}
+          </p>
+          <input
+            type="text"
+            placeholder="Apodo"
+            className="p-2 rounded-md bg-peach border-gray-500 border w-full"
+          />
+          <button className="bg-pink p-2 rounded-lg text-white w-full">
+            Todo Listo
+          </button>
         </form>
       </section>
     </div>
