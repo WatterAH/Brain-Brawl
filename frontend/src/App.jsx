88f56393264d1ad6fc62fde4e709e17d4b571263
WatterAH <samueltlahuel.m@gmail.com>
2023-12-13 00:23:00 -0600

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const App = () => {
-  return <div className="bg-purple h-screen">App</div>;
+  return (
+    <div className="bg-purple h-screen flex justify-center items-center">
+      <section className="flex flex-col items-center">
+        <h1 className="text-5xl font-irish text-white">
+          Bienvenido a Brain Brawl
+        </h1>
+        <form
+          className="flex flex-col items-center justify-center bg-peach w-full max-w-xs"
+          action=""
+        >
+          <input type="text" placeholder="Apodo" />
+          <button className="bg-pink">Todo Listo</button>
+        </form>
+      </section>
+    </div>
+  );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -7,6 +9,10 @@ export default {
         purple: "#937DC2",
         pink: "#E8A0BF",
         peach: "#F5CAB3",
+      },
+      fontFamily: {
+        montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        irish: ['"Irish Grover"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
